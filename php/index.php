ddc06e0fcfccd31df8b563d35693409aeda864df
Tangui Morlier <tangui@tangui.eu.org>
2020-11-13 18:42:28 +0100

    Prediction when data are received

--- php/index.php
+++ php/index.php
@@ -2,7 +2,11 @@
 
 $post = file_get_contents('php://input');
 if ($post) {
-	file_put_contents("data/triphasee_".date(DATE_ATOM).".wifi", $post);
+	$file = "data/triphasee_".date(DATE_ATOM).".wifi";
+	file_put_contents($file, $post);
+	$f = popen("bash ../prediction/wifi2localization.sh > ".$file.".prediction",'w');
+	fwrite($f, file_get_contents($file));
+	pclose($f);
 	exit;
 }
 header("Location: list_files.php");
